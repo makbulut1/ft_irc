da0c3fd425d8124bd7d97e86ea4a13ac56fc0544
Ali Furkan <ftekdrmi@k1m42s01.42istanbul.com.tr>
2023-01-12 18:37:48 +0300

    notice command changed and edited

--- src/commands/Notice.cpp
+++ src/commands/Notice.cpp
@@ -3,11 +3,16 @@
 int notice(int id, std::vector<struct pollfd> &pfd, std::vector<Client> &usr,std::string data)
 {
 	std::string message;
+	int i = -1;
+	int cliOfNbr;
 
 	(void)usr;
 	(void)data;
-	message = "[FT_IRC] Have a fun in the Server! :)\nDon't forget to use the [HELP] command if you need help.\r\n";
-	SendMessage(message, pfd[id].fd);
+	cliOfNbr = pfd.size();
+	message = usr[id - 1].getNick() + " Send a message to everyone: " + data + "\r\n";
+	//message = "[FT_IRC] Have a fun in the Server! :)\nDon't forget to use the [HELP] command if you need help.\r\n";
+	while(++i <= cliOfNbr - 1)
+		SendMessage(message, pfd[i].fd);
 
 	return 0;
 }
