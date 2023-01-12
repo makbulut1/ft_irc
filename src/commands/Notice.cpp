4e7cbc24273abb1cb93a34d410bb5fab97503fae
Ali Furkan <ftekdrmi@k1m42s01.42istanbul.com.tr>
2023-01-12 20:38:28 +0300

    notice changed and edited

--- src/commands/Notice.cpp
+++ src/commands/Notice.cpp
@@ -10,9 +10,7 @@ int notice(int id, std::vector<struct pollfd> &pfd, std::vector<Client> &usr,std
 	(void)data;
 	cliOfNbr = pfd.size();
 	message = usr[id - 1].getNick() + " Send a message to everyone: " + data + "\r\n";
-	//message = "[FT_IRC] Have a fun in the Server! :)\nDon't forget to use the [HELP] command if you need help.\r\n";
 	while(++i <= cliOfNbr - 1)
 		SendMessage(message, pfd[i].fd);
-
 	return 0;
 }
