acb39f778da2c77f1f20d9209fcda9442eb2ee68
Ali Furkan <ftekdrmi@k1m42s01.42istanbul.com.tr>
2023-01-14 15:09:23 +0300

    notice auth login control added

--- src/commands/Notice.cpp
+++ src/commands/Notice.cpp
@@ -3,11 +3,15 @@
 int notice(int id, std::vector<struct pollfd> &pfd, std::vector<Client> &usr,std::string data)
 {
 	std::string message;
+	if( usr[id - 1].getNickStat() == 0 || usr[id - 1].getNameStat() == 0 )
+	{
+		message = "You have not a nick or username!\r\n";
+		SendMessage(message, pfd[id].fd);
+		return 0;
+	}
 	int i = -1;
 	int cliOfNbr;
 
-	(void)usr;
-	(void)data;
 	cliOfNbr = pfd.size();
 	message = usr[id - 1].getNick() + " Send a message to everyone: " + data + "\r\n";
 	while(++i <= cliOfNbr - 1)
